605f8bfcf4238972b58b6eef9fccaffc521957b1
IldiBustamante <ild.sweet@hotmail.com>
2019-05-22 20:49:46 -0300

    agrego nombres

---
+++ script.js
@@ -0,0 +1,9 @@
+//hay primitivos y objetos
+// primitivos number, string, boolean, los que vamos a usar
+// objetos : object, array y function
+// hay variables
+
+let nombre="Ildara";
+let apellido="Bustamante"
+let nombre_completo= nombre + " "+  apellido
+console.log(nombre_completo);
